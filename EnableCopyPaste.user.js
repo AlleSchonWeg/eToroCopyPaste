33ab68f658240abb5d5dc2be61916516a091b909
AlleSchonWeg <6727366+AlleSchonWeg@users.noreply.github.com>
2019-03-05 13:14:07 +0100

    Show all trades

--- EnableCopyPaste.user.js
+++ EnableCopyPaste.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         eToro Copy & Paste
 // @namespace    Disable Etoro C&P Restrictions
-// @version      1.1
+// @version      1.2
 // @description  eToro Copy & Paste is a TamperMonkey script to enable copy & paste functionality to eToro
 // @author       S99
 // @match        https://www.etoro.com/*
@@ -17,7 +17,7 @@ waitForKeyElements("div.user-head-content-ph",pageLoaded);
 waitForKeyElements("portfolio-list-view",pageLoaded);
 waitForKeyElements("div.portfolio-history-grid",pageLoaded);
 waitForKeyElements("div.portfolio-table-view",pageLoaded);
-
+waitForKeyElements("body > ui-layout > div > div > div.main-app-view.ng-scope > div > div.w-portfolio-table.portfolio-open-trades-inner.head > div > div > div.e-btn.light.i-ptc-action.edit-extended.dropdown-menu.ng-scope.ng-isolate-scope",settingsVisible);
 
 function enableSelect() {
     $('body').css({
@@ -37,4 +37,10 @@ function enableSelect() {
 }
 function pageLoaded(jNode) {
     enableSelect();
+}
+function settingsVisible(jNode) {
+    angular.element("body > ui-layout > div > div > div.main-app-view.ng-scope > div > div.w-portfolio-table.portfolio-open-trades-inner.head > div > div > div.e-btn.light.i-ptc-action.edit-extended.dropdown-menu.ng-scope.ng-isolate-scope").click();
+    angular.element("body > ui-layout > div > div > div.main-app-view.ng-scope > div > div.w-portfolio-table.portfolio-open-trades-inner.head > div > div > div.e-btn.light.i-ptc-action.edit-extended.dropdown-menu.ng-scope.ng-isolate-scope.open > div > div > div.drop-select-box-option.pointer.one-click-trading.ng-scope > span").click();
+    angular.element("body > ui-layout > div > div > div.main-app-view.ng-scope > div > div.w-portfolio-table.portfolio-open-trades-inner.head > div > div > div.e-btn.light.i-ptc-action.edit-extended.dropdown-menu.ng-scope.ng-isolate-scope.open").click();
+    
 }
